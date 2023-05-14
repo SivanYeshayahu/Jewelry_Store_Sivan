2a7907680ad9b8cdf539ed3c6d5bbda00303a83d
Sivan <63412158+SivanYeshayahu@users.noreply.github.com>
2023-05-14 21:46:56 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!--6.FOTTER:-->
+<div class="footer ">
+
+
+    <a class="footer-links" href="https://github.com/sivanyeshayahu"> 2020© sivan yeshayahu</a>
+
+</div>
+
+
+</body>
+</html><!--END OF THE PAGE-->
+
